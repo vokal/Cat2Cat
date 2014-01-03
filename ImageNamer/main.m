580ca396d5354fc2ae28c9affaf119fd681bd861
Ellen Shapiro <designatednerd@gmail.com>
2014-01-03 12:53:59 -0600

    Initial Commit

---
+++ ImageNamer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ImageNamer
+//
+//  Created by Ellen Shapiro (Vokal) on 1/3/14.
+//  Copyright (c) 2014 Vokal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
