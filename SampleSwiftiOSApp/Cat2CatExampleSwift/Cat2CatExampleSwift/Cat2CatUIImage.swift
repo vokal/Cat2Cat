3d88f1d0efde7352e3eae531e0cdf3eb60997070
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2014-10-29 15:29:18 -0500

    Constructing Swift-iOS example project, based on ObjC-iOS example project.

---
+++ SampleSwiftiOSApp/Cat2CatExampleSwift/Cat2CatExampleSwift/Cat2CatUIImage.swift
@@ -0,0 +1,52 @@
+//
+// Cat2CatUIImage.swift
+//
+// Generated Automatically Using Cat2Cat
+// NOTE: If you edit this file manually, your changes will be overrwritten the next time this app runs.
+//
+// For more information, go to http://github.com/VokalInteractive/Cat2Cat
+//
+
+import UIKit
+
+extension UIImage {
+
+    // MARK: - ICONS
+    
+    // MARK: - Public Domain Icons
+    
+    class func ac_No_C() -> UIImage {
+        return UIImage(named:"No@C")
+    }
+    
+    class func ac_PD_in_circle() -> UIImage {
+        return UIImage(named:"PD in circle")
+    }
+    
+    class func ac_PDe_Darka_Circle() -> UIImage {
+        return UIImage(named:"PDéDarkåCircle")
+    }
+    
+    class func ac_SidewaysC() -> UIImage {
+        return UIImage(named:"SidewaysC")
+    }
+    
+    // MARK: - PHOTOS
+    
+    class func ac_Golden_Gate_Bridge() -> UIImage {
+        return UIImage(named:"Golden Gate Bridge")
+    }
+    
+    class func ac_US_Capitol() -> UIImage {
+        return UIImage(named:"US Capitol")
+    }
+    
+    class func ac_Venice_Beach() -> UIImage {
+        return UIImage(named:"Venice Beach")
+    }
+    
+    class func ac_Wrigley_Field() -> UIImage {
+        return UIImage(named:"Wrigley Field")
+    }
+    
+}
