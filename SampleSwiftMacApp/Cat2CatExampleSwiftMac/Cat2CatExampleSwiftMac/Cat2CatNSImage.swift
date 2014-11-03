46b6fbdf611e663c4dff39849f6f0b95d37d89f0
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2014-11-03 13:00:23 -0600

    Adding Mac Swift sample project.

---
+++ SampleSwiftMacApp/Cat2CatExampleSwiftMac/Cat2CatExampleSwiftMac/Cat2CatNSImage.swift
@@ -0,0 +1,60 @@
+//
+// Cat2CatNSImage.swift
+//
+// Generated Automatically Using Cat2Cat
+// NOTE: If you edit this file manually, your changes will be overrwritten the next time this app runs.
+//
+// For more information, go to http://github.com/VokalInteractive/Cat2Cat
+//
+
+import AppKit
+
+extension NSImage {
+
+    // MARK: - ICONS
+    
+    class func ac_AppIcon() -> NSImage? {
+        return NSImage(named:"AppIcon")
+    }
+    
+    class func ac_Icon() -> NSImage? {
+        return NSImage(named:"Icon")
+    }
+    
+    // MARK: - Public Domain Icons
+    
+    class func ac_No_C() -> NSImage? {
+        return NSImage(named:"No@C")
+    }
+    
+    class func ac_PD_in_circle() -> NSImage? {
+        return NSImage(named:"PD in circle")
+    }
+    
+    class func ac_PDe_Darka_Circle() -> NSImage? {
+        return NSImage(named:"PDéDarkåCircle")
+    }
+    
+    class func ac_SidewaysC() -> NSImage? {
+        return NSImage(named:"SidewaysC")
+    }
+    
+    // MARK: - PHOTOS
+    
+    class func ac_Golden_Gate_Bridge() -> NSImage? {
+        return NSImage(named:"Golden Gate Bridge")
+    }
+    
+    class func ac_US_Capitol() -> NSImage? {
+        return NSImage(named:"US Capitol")
+    }
+    
+    class func ac_Venice_Beach() -> NSImage? {
+        return NSImage(named:"Venice Beach")
+    }
+    
+    class func ac_Wrigley_Field() -> NSImage? {
+        return NSImage(named:"Wrigley Field")
+    }
+    
+}
