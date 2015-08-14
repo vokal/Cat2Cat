b1e1518ab036c62d6a4f2ffa323442752f3e8db8
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2015-08-14 15:34:02 -0500

    Add zip zap pod.

--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ platform :osx, "10.9"
 
 pod 'GRMustache', '~> 7.3.0'
 pod 'BRLOptionParser', :git => 'https://github.com/vokal-isaac/BRLOptionParser.git' #'~> 0.3.1'
+pod 'zipzap', '8.0.3'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -24,3 +24,27 @@
 #define COCOAPODS_VERSION_MINOR_JRSwizzle 0
 #define COCOAPODS_VERSION_PATCH_JRSwizzle 0
 
+// zipzap
+#define COCOAPODS_POD_AVAILABLE_zipzap
+#define COCOAPODS_VERSION_MAJOR_zipzap 8
+#define COCOAPODS_VERSION_MINOR_zipzap 0
+#define COCOAPODS_VERSION_PATCH_zipzap 3
+
+// zipzap/c++
+#define COCOAPODS_POD_AVAILABLE_zipzap_c__
+#define COCOAPODS_VERSION_MAJOR_zipzap_c__ 8
+#define COCOAPODS_VERSION_MINOR_zipzap_c__ 0
+#define COCOAPODS_VERSION_PATCH_zipzap_c__ 3
+
+// zipzap/objc
+#define COCOAPODS_POD_AVAILABLE_zipzap_objc
+#define COCOAPODS_VERSION_MAJOR_zipzap_objc 8
+#define COCOAPODS_VERSION_MINOR_zipzap_objc 0
+#define COCOAPODS_VERSION_PATCH_zipzap_objc 3
+
+// zipzap/objc++
+#define COCOAPODS_POD_AVAILABLE_zipzap_objc__
+#define COCOAPODS_VERSION_MAJOR_zipzap_objc__ 8
+#define COCOAPODS_VERSION_MINOR_zipzap_objc__ 0
+#define COCOAPODS_VERSION_PATCH_zipzap_objc__ 3
+
